eb815af918e9220439d4ab92ea47072da6ed3b9b
usaamerica282008-alt <usaamerica282008@gmail.com>
2026-07-17 20:48:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,10 @@ def webhook():
                 print("Error sending message from bot:", e)
                 
     return "OK", 200
+    @app.route('/')
+def home():
+    return "Bot is running successfully with 200 OK!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
