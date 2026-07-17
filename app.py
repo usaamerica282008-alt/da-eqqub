866786d8acba44a68cdeb4d272d597c27d6bb3f4
usaamerica282008-alt <usaamerica282008@gmail.com>
2026-07-17 22:26:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,9 @@ def webhook():
         return "Bot is running successfully with 200 OK!", 200
 
 if __name__ == "__main__":
-            import os
-        port = int(os.environ.get("PORT", 5000))
-            app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
