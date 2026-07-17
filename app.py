9a6077be3877f9ee33b8063a2bcf570fc3b32403
usaamerica282008-alt <usaamerica282008@gmail.com>
2026-07-17 21:18:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,9 +43,8 @@ def webhook():
                 
     return "OK", 200
     @app.route('/')
-def home():
-    return "Bot is running successfully with 200 OK!", 200
-
+    def home():
+        return "Bot is running successfully with 200 OK!", 200
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
